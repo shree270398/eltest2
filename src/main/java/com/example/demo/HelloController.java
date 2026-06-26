0cbb2b592e1e725bd1536ca85626ac8c2f64cae3
shree270398 <shreenivas0327@gmail.com>
2026-06-26 13:37:41 +0530

    third

---
+++ src/main/java/com/example/demo/HelloController.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Hello from Spring Boot running on Kubernetes!";
+    }
+    @GetMapping("/demo1")
+    public String demo(){
+        return "demo1";
+    }
+}
